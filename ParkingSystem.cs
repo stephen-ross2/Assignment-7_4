9fa98c8ed956f174310d7e2362364250f0cb07d5
stephen-ross2 <stephen.ross2@outlook.com>
2024-12-19 10:04:33 -0600

    Add project files.

---
+++ ParkingSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7_4
+{
+    public class ParkingSystem
+    {
+        private Dictionary<int, int> parkingSpaces;
+               
+        public ParkingSystem(int big, int medium, int small)
+        {
+            parkingSpaces = new Dictionary<int, int>
+            {
+                {1, big},
+                {2, medium},
+                {3, small}
+            };
+
+        }
+              
+        public bool AddCar(int carType)
+        {
+            if (parkingSpaces.ContainsKey(carType) && parkingSpaces[carType] > 0)
+            {
+                parkingSpaces[carType]--;
+                return true;
+            }
+            return false; 
+        }
+    }
+}
